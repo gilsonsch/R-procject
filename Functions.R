6c4121f44bc87beb02acdd952b8b5cfcb3162843
Luis Robles <lroblesr@unmsm.edu.pe>
2017-07-15 18:25:40 -0500

    datos en clase

--- Functions.R
+++ Functions.R
@@ -1,6 +1,62 @@
 # intro a fun
 
+v <- rnorm(20)
 
+sum(v)/length(v)
 
+mean(v)
 
 # examples
+
+
+mi_funcion <- function(a,b){
+  c <- sqrt(a^2 + b^2)
+  return(c)
+}
+
+test <- mi_funcion(a=3,b=4)
+  
+mi_funcion(3,4)
+
+
+mi_funcion1 <- function(a,b){
+  c <- sqrt(a^3 + b^2)
+  return(c)
+}
+
+mi_funcion1(2,3)
+mi_funcion1(b=3,a=2)
+
+# vec <- 10
+#########################
+paquete <- function(pkg){
+  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new.pkg))
+    install.packages(new.pkg, dependencies = TRUE)
+  sapply(pkg, require, character.only = TRUE)
+}
+
+library()
+paquete("pack01","pack02")
+
+install.package("paquete", dep=T)
+
+
+Sys.time()
+
+hora <- function(){
+  paste("=>",format(Sys.time(),"%X"),"|",Sys.Date())
+}
+hora()
+
+
+
+# crop raster data
+miFUN <- function(dato,vector){
+  
+}
+
+
+
+
+  
--- Time_series.R
+++ Time_series.R
@@ -1,16 +1,44 @@
 # reading data
+# install.packages("raster",dep=T)
+library(raster)
 
+data1 <- raster("Data/NDVI_2000_03.tif")
+ls()
+class(data1)
 
+# install.packages("raster_2.5-8.zip",repos = NULL, type = "win.binary")
 # plotting data
-
+plot(data1)
 
 # reading shape file
-
+shp <- shapefile("SHP/Lamb.shp")
+ls()
 
 # plotting shape file
+plot(shp)
 
+plot(data1)
+plot(shp, add=T)
+plot(shp, add=T,col="red")
+
+
+plot(data1)
+plot(shp,add=T, border="red")
+
+extent(shp)
+
+# testing <- extent(0, 20, 0, 20)
+
+EE <- drawExtent()
+EE
 
 # cropping data
+data_Crop <- crop(data1,EE)
+# plot(data_Crop)
+
+crop_shape <- mask(data1,shp)
+plot(crop_shape)
+
 
 
 # extract time series
